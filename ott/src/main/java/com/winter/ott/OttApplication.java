3c61da1b1801a309ff028c9f741762ab3e4ecf85
youngjaeee <youngjaeee@chungbuk.ac.kr>
2022-02-09 18:22:58 +0900

    [#1] 프로젝트 기초 작업

---
+++ ott/src/main/java/com/winter/ott/OttApplication.java
@@ -0,0 +1,13 @@
+package com.winter.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OttApplication.class, args);
+    }
+
+}
---
+++ ott/src/test/java/com/winter/ott/OttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winter.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
